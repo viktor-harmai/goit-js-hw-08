51175223f10617f519baa1f85c56b4bcc1741d3d
viktor-harmai <vicg907@gmail.com>
2022-08-06 13:13:42 +0300

    Task-01-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+//================================================================
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.galleryEl.innerHTML = galleryMarkup;
+// refs.galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+
+        <a class="gallery__item" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+        </a>
+        `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery .gallery__item', {
+  overlayOpacity: 1,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
