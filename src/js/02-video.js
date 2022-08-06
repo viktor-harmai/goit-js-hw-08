29e19c32e94bc510b1e93aa4fe25ca765525bec8
viktor-harmai <vicg907@gmail.com>
2022-08-06 16:13:58 +0300

    Task-02-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+//отслеживаем событие timeupdate вызываем функцию onPlay раз в секунду
+// и сохраняем время в localStorage
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+// возобновляем воспроизведения с сохраненной позиции.
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
